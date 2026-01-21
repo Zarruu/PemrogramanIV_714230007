88d611bb3acc695745a97a5112d75775943f81f2
Rapz treamer <nizarakmal8@gmail.com>
2026-01-21 09:26:18 +0700

    up p14

---
+++ Pertemuan14/Praktikum/dio_contact/lib/view/screen/login_page.dart
@@ -0,0 +1,156 @@
+import 'package:dio_contact/model/login_model.dart';
+import 'package:dio_contact/services/api_services.dart';
+import 'package:dio_contact/services/auth_manager.dart';
+import 'package:dio_contact/view/screen/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  final ApiServices _dataService = ApiServices();
+
+  @override
+  void initState() {
+    super.initState();
+    checkLogin();
+  }
+
+  void checkLogin() async {
+    bool loggedIn = await AuthManager.isLoggedIn();
+    if (loggedIn) {
+      Navigator.pushAndRemoveUntil(
+        context, 
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+      ), 
+      (route) => false,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  String? _validateUsername(String? value) {
+    if (value != null && value.length < 4) {
+      return 'Masukkan minimal 4 karakter';
+    }
+    return null;
+  }
+
+  String? _validatePassword(String? value) {
+    if (value != null && value.length < 3) {
+      return 'Masukkan minimal 3 karakter';
+    }
+    return null;
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Login Page')),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: _validateUsername,
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.account_circle_rounded),
+                        hintText: 'Write username here...',
+                        labelText: 'Username',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        fillColor: Color.fromARGB(255, 242, 254, 255),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      obscureText: true,
+                      controller: _passwordController,
+                      validator: _validatePassword,
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.lock_rounded),
+                        hintText: 'Write password here...',
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        fillColor: Color.fromARGB(255, 242, 254, 255),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        final isValidForm = _formKey.currentState!.validate();
+                        if (isValidForm) {
+                          final postModel = LoginInput(
+                            username: _usernameController.text, 
+                            password: _passwordController.text,
+                            );
+
+                            LoginResponse? res = 
+                            await _dataService.login(postModel);
+                            if (res!.status == 200) {
+                              await AuthManager.login(
+                                _usernameController.text);
+                              Navigator.pushAndRemoveUntil(
+                                context, 
+                                MaterialPageRoute(
+                                  builder: (context) => const HomePage(),
+                                ), 
+                                (route) => false,
+                              );
+                            } else {
+                              displaySnackbar(res.message);
+                            }
+                        }
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  dynamic displaySnackbar(String msg) {
+    return ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(msg)));
+  }
+}
